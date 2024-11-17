193db114e6a2471eec259fcad6fa8faebf04e02b
Colevy Collins <77639915+Colevy-Collins@users.noreply.github.com>
2024-11-17 17:14:41 -0500

    update progress files

--- time_keeper/test/widget_test.dart
+++ time_keeper/test/widget_test.dart
@@ -7,55 +7,24 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:time_keeper/main.dart';
-import 'package:integration_test/integration_test.dart';
-
-
 
 void main() {
-
-
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
-    // Tap the 'Add Task' button and trigger a frame.
-    await tester.tap(find.text('Add Task'));
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that the dialog is displayed.
-    await expectLater(find.text('Submit Task'), findsOneWidget);
-
-    // Enter task details in the dialog.
-    await tester.enterText(find.byType(TextField).at(0), 'today');
-    await tester.enterText(find.byType(TextField).at(1), '10:00');
-    await tester.enterText(find.byType(TextField).at(2), '11:00');
-    await tester.enterText(find.byType(TextField).at(3), 'Test Task');
-    await tester.enterText(find.byType(TextField).at(4), 'Test Tag');
-
-    // Verify that the task details are entered.
-    await expectLater(find.text('today'), findsOneWidget);
-    await expectLater(find.text('10:00'), findsOneWidget);
-    await expectLater(find.text('11:00'), findsOneWidget);
-    await expectLater(find.text('Test Task'), findsOneWidget);
-    await expectLater(find.text('Test Tag'), findsOneWidget);
-
-    // Submit the task.
-    await tester.tap(find.text('Submit Task'));
-    await tester.pump();
-
-    // Verify that the message text widget has the right text.
-    await expectLater(find.byKey(Key('messageText')), findsOneWidget);
-    await expectLater(find.text('Task submitted: Test Task from 10:00 to 11:00 on today with tag Test Tag.'), findsOneWidget);
-
-    // Verify that the dialog is closed.
-    await expectLater(find.text('Submit Task'), findsNothing);
-
-    // Verify that the task is in the database.
-    await tester.tap(find.text('Show All Tasks'));
-    await tester.pump();
-
-    // Verify that the task is displayed in the list.
-    //await expectLater(find.byWidgetPredicate((widget) => widget is Text && widget.data == 'Test Task'), findsOneWidget);
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
